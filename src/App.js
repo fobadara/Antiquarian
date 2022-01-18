20f20a8c98a4bc5b35b45d342bf514a40a6dc394
Abdulfatai <fob90s@gmail.com>
2022-01-18 11:00:32 +0100

    Make changes to code from previous branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Books from './components/books';
+import Categories from './components/categories';
+import Header from './components/header';
 import './App.css';
+
+const temporaryProp = [
+  { id: 1, title: 'I need API. I am ready for data.' },
+  { id: 2, title: 'I need API. I am ready for data.' },
+];
 
 function App() {
   return (
-    <div className="App" />
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Books books={temporaryProp} />} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './app/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <StrictMode>
+    <Router>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </Router>
+  </StrictMode>,
   document.getElementById('root'),
 );
 
