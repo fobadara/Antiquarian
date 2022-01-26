068b679f2e86b66ab53874f6498837991ae36540
Abdulfatai <fob90s@gmail.com>
2022-01-26 12:33:28 +0100

    Add redux

--- src/components/addbook.js
+++ src/components/addbook.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/books';
@@ -5,21 +6,35 @@ import { addBook } from '../redux/books/books';
 const AddBook = () => {
   const dispatch = useDispatch();
 
-  const submitBookToStore = () => {
-    const newBook = {
-      id: uuidv4(),
-      title: 'I need API. I am ready for data.',
-      author: 'human',
-    };
+  const [state, setState] = useState({
+    item_id: '',
+    title: '',
+    author: '',
+    category: '',
+  });
 
-    dispatch(addBook(newBook));
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const submitBookToStore = (event) => {
+    event.preventDefault();
+    setState({ ...state, item_id: uuidv4() });
+    console.log(state)
+    dispatch(addBook(state));
+    // const newBook = {    dispatch(addBook(newBook));
+
+    //   id: uuidv4(),
+    //   title: 'I need API. I am ready for data.',
+    //   author: 'human',
+    // };
   };
 
   return (
     <>
       <h2>Add New Book</h2>
       <form action="#">
-        <input type="text" required />
+        <input type="text" name="title" onChange={handleChange} required />
         <button type="submit" onClick={submitBookToStore}>Add Book</button>
       </form>
     </>
